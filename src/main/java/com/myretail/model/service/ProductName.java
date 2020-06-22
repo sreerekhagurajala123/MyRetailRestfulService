c66db4bd86c5efd24da91444e76e14c0c92305c6
sreerekha <sreerekha.gurajala@cerner.com>
2020-06-22 11:57:02 +0530

    Implementation of GET and PUT API's for myretail restful service and Junits

---
+++ src/main/java/com/myretail/model/service/ProductName.java
@@ -0,0 +1,20 @@
+package com.myretail.model.service;
+
+/**
+ * This class represents the object containing Product Name.
+ * 
+ * @author SreeRekha Gurajala
+ *
+ */
+public class ProductName {
+	private String productName;
+
+	public String getProductName() {
+		return productName;
+	}
+
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+
+}
